Fix bug on calculation on num of all train data; add schemes for FedProx

flmod/solvers/fedavg_schemes.py
@@ -15,6 +15,7 @@ class FedAvgSchemes(BaseFedarated):
         self.clients_select_prob = self.get_clients_prob()  # p_k
         self.scheme = options["scheme"]  # scheme 1,2, transformed 2
         assert self.num_clients == 100, '客户端数量是100'
+        assert self.scheme in ['1', '2', '2t'], "1 for scheme I; 2 for scheme II; 2t for transformed scheme II"
 
     def get_clients_prob(self):
         num_alldata = []
@@ -37,23 +38,30 @@ class FedAvgSchemes(BaseFedarated):
                 select_index.append(i)
                 repeated_times.append(1)
             else:
-                # 对应的顺序(即对应的客户端的index)出现的次数
+                # 对应的顺序(即对应的客户端的index)出现的次数. 是为了保障只调用一次client
                 repeated_times[-1] += 1
         return select_clients, repeated_times
 
+    @property
+    def is_scheme2_like(self):
+        return self.scheme.startswith('2')
+
     def aggregate(self, solns, **kwargs):
         averaged_solution = torch.zeros_like(self.latest_model)
-        # p_k*(N/K) * W_k
-        if self.scheme.startswith('2'):
+        if self.is_scheme2_like:
             # scheme 2; transformed scheme 2
+            # \sum_k{p_k* * W_k} * N / K, p_k = 1 / N
+            # total_num = 0
             for num_sample, local_solution in solns:
-                averaged_solution += local_solution * num_sample  # simply sum the flatten parameters
+                averaged_solution += local_solution  # 和原文不一样. 这里没有乘以样本数量
+                # total_num += num_sample
             averaged_solution /= self.num_train_data  # 这里是总共的数据量,不是运行客户端的数量
             averaged_solution *= (self.num_clients / self.clients_per_round)
         else:
             # scheme 1
+            # \sum_k{W_k} / K
             repeated_times = kwargs['repeated_times']
-            assert repeated_times == len(solns)
+            assert len(repeated_times) == len(solns)
             for i, (num_sample, local_solution) in enumerate(solns):
                 averaged_solution += local_solution * repeated_times[i]
             averaged_solution /= self.clients_per_round  # TODO 一般情况下 clients_per_round = min(self.clients_per_round, self.num_clients)
@@ -69,7 +77,7 @@ class FedAvgSchemes(BaseFedarated):
             if (round_i + 1) % self.eval_on_test_every_round == 0:
                 self.test_latest_model_on_evaldata(round_i)
 
-            if self.scheme.startswith('2'):
+            if self.is_scheme2_like:
                 selected_clients, repeated_times = self.select_clients(round=round_i, num_clients=self.clients_per_round), None
             else:
                 selected_clients, repeated_times = self.select_clients_with_prob(round_i)

flmod/solvers/fedprox.py
@@ -9,15 +9,37 @@ from flmod.optimizers.pgd import PerturbedGradientDescent
 class FedProx(BaseFedarated):
 
     def __init__(self, options, all_data_info):
+        """
+        FedProx
+        原始论文给出的 tf 的代码的实现不完全相同, 主要的区别在于:
+        1. 选取设备的方式(tf代码是完全随机的)
+        2. 合并权重的时候除以了运行的客户端总数 K
+        :param options:
+        :param all_data_info:
+        """
         model, crit = choose_model_criterion(options=options)
         self.optimizer = PerturbedGradientDescent(model.parameters(), lr=options['lr'], mu=options['mu'], weight_decay=options['wd'])
-        suffix = f'mu{options["mu"]}_dp{options["drop_rate"]}'
+        self.scheme = 'unfo_prob' if options['scheme'] == '' else options['scheme']
+        # FedProx 也接受 scheme 参数, 含义如下
+        assert self.scheme in ['prob_simp', 'unfo_prob'], 'prob_simp for sampling clients with their probilities and simply average the weights; unfo_prob(default): uniformly chose clients and average weight by prob'
+
+        suffix = f'mu{options["mu"]}_dp{options["drop_rate"]}_scheme[{self.scheme}]'
         super(FedProx, self).__init__(options=options, model=model, dataset=all_data_info, optimizer=self.optimizer,
                                      criterion=crit, append2metric=suffix)
         self.drop_rate = options['drop_rate']
-        # 保存当前的模型的参数, 在聚合之前的
+        self.prob = self.get_clients_prob()
 
-    def select_clients(self, round, num_clients=20):
+    def get_clients_prob(self):
+        num_alldata = []
+        for client in self.clients:
+            num_alldata.append(client.num_train_data)
+        assert sum(num_alldata) == self.num_train_data
+        return np.array(num_alldata) / sum(num_alldata)
+
+    def prob_select_simple_average(self):
+        return self.scheme == 'prob_simp'
+
+    def select_clients_with_prob(self, round, num_clients=20):
         """
         这里只输出客户端的编号和被选择的客户端
         :param round:
@@ -26,8 +48,27 @@ class FedProx(BaseFedarated):
         """
         num_clients = min(num_clients, self.num_clients)  # 选择的客户端
         np.random.seed(round)  # make sure for each comparison, we are selecting the same clients each round
-        selected_clients_indx = np.random.choice(self.num_clients, num_clients, replace=False)
-        return selected_clients_indx
+        target_clients = []
+        client_indices = []
+        multiple_times = []
+        selected_clients_indx = np.random.choice(self.num_clients, num_clients, replace=True, p=self.prob)
+        selected_clients_indx = sorted(selected_clients_indx)
+        for i in selected_clients_indx:
+            if i not in client_indices:
+                client_indices.append(i)
+                multiple_times.append(1)
+                target_clients.append(self.clients[i])
+            else:
+                # 注意这是有序的
+                multiple_times[-1] += 1
+        return target_clients, multiple_times, client_indices
+
+    def aggregate_simply(self, solns, multiple_times):
+        averaged_solution = torch.zeros_like(self.latest_model)
+        for i, (num_sample, local_solution) in enumerate(solns):
+            averaged_solution += local_solution * multiple_times[i]
+        averaged_solution /= self.clients_per_round  # TODO 一般情况下 clients_per_round = min(self.clients_per_round, self.num_clients)
+        return averaged_solution.detach()
 
     def local_train(self, selected_clients, round_i):
         raise NotImplementedError
@@ -53,27 +94,50 @@ class FedProx(BaseFedarated):
             if (round_i + 1) % self.eval_on_train_every_round == 0:
                 # 仅仅计算梯度
                 self.calc_client_grads(round_i)
-            selected_clients_indices = self.select_clients(round=round_i, num_clients=self.clients_per_round)
-            activated_clients_indx = np.random.choice(selected_clients_indices, round(self.clients_per_round * (1 - self.drop_rate)), replace=False)
-            # 能够顺利完成任务客户端, 其余的不在 active 但是被选择的客户端被视为 starggle
-            self.update_optimizer_weights()
-            solns = []
-            stats = []
-            for selected_i in selected_clients_indices:
-                c = self.clients[selected_i]
-                c.set_flat_model_params(self.latest_model)
-                if selected_i in activated_clients_indx:
-                    # 运行相同的 num_epoch
-                    soln, stat = c.local_train(round_i, self.num_epochs)
-                else:
-                    soln, stat = c.local_train(round_i, num_epochs=np.random.randint(low=1, high=self.num_epochs))
-                solns.append(soln)
-                stats.append(stat)
+            if self.prob_select_simple_average():
+                target_clients, multiple_times, client_indices = self.select_clients_with_prob(round_i, self.clients_per_round)
+                # 那些客户端的编号可以完成所有的操作
+                activated_clients_indx = np.random.choice(client_indices,
+                                                          round(self.clients_per_round * (1 - self.drop_rate)),
+                                                          replace=False)
+                # 能够顺利完成任务客户端, 其余的不在 active 但是被选择的客户端被视为 starggle
+                self.update_optimizer_weights()
+                solns = []
+                stats = []
+                for client_index, client in zip(client_indices, target_clients):
+                    client.set_flat_model_params(self.latest_model)
+                    if client_index in activated_clients_indx:
+                        # 运行相同的 num_epoch
+                        soln, stat = client.local_train(round_i, self.num_epochs)
+                    else:
+                        soln, stat = client.local_train(round_i, num_epochs=np.random.randint(low=1, high=self.num_epochs))
+                    solns.append(soln)
+                    stats.append(stat)
+                self.latest_model = self.aggregate_simply(solns, multiple_times)
+            else:
+                selected_clients_indices = self.select_clients(round=round_i, num_clients=self.clients_per_round)
+                activated_clients_indx = np.random.choice(selected_clients_indices,
+                                                          round(self.clients_per_round * (1 - self.drop_rate)),
+                                                          replace=False)
+                # 能够顺利完成任务客户端, 其余的不在 active 但是被选择的客户端被视为 starggle
+                self.update_optimizer_weights()
+                solns = []
+                stats = []
+                for selected_i in selected_clients_indices:
+                    c = self.clients[selected_i]
+                    c.set_flat_model_params(self.latest_model)
+                    if selected_i in activated_clients_indx:
+                        # 运行相同的 num_epoch
+                        soln, stat = c.local_train(round_i, self.num_epochs)
+                    else:
+                        soln, stat = c.local_train(round_i, num_epochs=np.random.randint(low=1, high=self.num_epochs))
+                    solns.append(soln)
+                    stats.append(stat)
+                self.latest_model = self.aggregate(solns)
 
             self.metrics.extend_commu_stats(round_i, stats)
 
             # update global weights
-            self.latest_model = self.aggregate(solns)
             if (round_i + 1) % self.save_every_round == 0:
                 self.save_model(round_i)
                 self.metrics.write()